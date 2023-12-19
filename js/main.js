c74adc03dcb76e94a9e209a76ffcf294339f648e
shubhambhalala <shubhambhalala@gmail.com>
2023-12-18 22:48:46 -0500

    skeleton page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('data/content.json')
+        .then(response => response.json())
+        .then(data => {
+            // Example: Update content for Home page
+            if (window.location.pathname.endsWith("index.html")) {
+                document.getElementById('main-content').innerHTML = `
+                    <h1>${data.home.title}</h1>
+                    <p>${data.home.description}</p>
+                `;
+            }
+            // Similar logic for other pages
+        })
+        .catch(error => console.error('Error loading content:', error));
+});
+
